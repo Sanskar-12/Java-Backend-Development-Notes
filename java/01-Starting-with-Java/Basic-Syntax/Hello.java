370fbc711cb7070d7bfbcb3d235b12ee03c7dc95
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-14 17:13:39 +0530

    Basic syntax and variables

---
+++ java/01-Starting-with-Java/Basic-Syntax/Hello.java
@@ -0,0 +1,11 @@
+class Hello {
+    public static void main(String args[]) {
+        System.out.println("Hello world");
+    }
+}
+
+// JDK : its a java development kit which gives java development tools like
+// debugger and compiler, etc
+// JRE : its a java runtime environment, the java runs with the help of this JRE
+// JVM : its a java virtual machine, which converts the bytecode to machine code
+// and its machine indepedent
---
+++ java/01-Starting-with-Java/Variables and Datatypes/Variables.java
@@ -0,0 +1,10 @@
+// Variables are containers where we can store values temporarily
+public class Variables {
+    public static void main(String args[]) {
+        int num1 = 2;
+        int num2 = 4;
+        int result = num1 + num2;
+
+        System.out.println(result);
+    }
+}
