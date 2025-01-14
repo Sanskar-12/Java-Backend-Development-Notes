2f3b9f3ca805af2242858182c289fc7b02cdbfec
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-14 18:01:00 +0530

    Datatypes and its sizes

---
+++ java/01-Starting-with-Java/Variables and Datatypes/Datatypes.java
@@ -0,0 +1,37 @@
+
+public class Datatypes {
+    public static void main(String args[]) {
+        // Primitive Datatypes
+        // 1. Integer -> int,long,short,byte
+        int a = 10;
+
+        // 2. Float -> float,double
+        double b = 3.14; // in java by default it uses double not float
+        float c = 3.14f; // explicitly define float
+
+        // 3. Character
+        char d = 'a';
+
+        // 4. Boolean
+        boolean e = true;
+        boolean f = false;
+    }
+}
+
+// Datatypes and its sizes
+
+// --> Primitive Data Types
+
+// a. Integer (1, -1, 2,...)
+// 1. int -> 4 bytes
+// 2. long -> 8 bytes
+// 3. short -> 2 bytes
+// 4. byte -> 1 byte
+
+// b. Float (by default used double) (2.1332, 3.34,...)
+// 1. float -> 4 bytes
+// 2. double -> 8 bytes
+
+// c. Character (a,b,c...) -> 2 bytes
+
+// d. Boolean (true or false)
