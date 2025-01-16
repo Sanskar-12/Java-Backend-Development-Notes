c53eb1ff36e85a1673e57fe4bb9f6a8a85845dd5
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-16 11:55:53 +0530

    Loops

---
+++ java/03-Loops-in-Java/Do-While-Loop/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        int i = 1;
+
+        // Do While loop
+        do {
+            System.out.println("Hello " + i);
+            i++;
+        } while (i <= 5);
+    }
+}
---
+++ java/03-Loops-in-Java/While-Loop/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        int i = 1;
+
+        // While loop
+        while (i <= 5) {
+            System.out.println("Hello " + i);
+            i++;
+        }
+    }
+}
