2c2f8be41299ff886f11b3bf495a46ff0e78846c
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-17 17:15:13 +0530

    All about Strings in java

---
+++ java/06-String-in-Java/Initialising-Strings/Demo.java
@@ -0,0 +1,21 @@
+
+public class Demo {
+    public static void main(String[] args) {
+        // One way of using String is below
+        // String str=new String("Sanskar");
+
+        // Or Other way of using it is below
+        String str = "Sanskar";
+
+        // Get the particular char of the string
+        System.out.println(str.charAt(0));
+
+        // Concatenate the words to the string
+        System.out.println(str.concat(" Vishwakarma"));
+
+        // Checks whether the ans contains in the String
+        System.out.println(str.contains("ans"));
+
+        // and there are many more methods in String
+    }
+}
