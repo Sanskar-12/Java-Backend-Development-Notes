3414afe47f0fc954ea23d7e03e58f36f0bb60341
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-21 11:51:57 +0530

    Abstract Keyword in Java

---
+++ java/13-Abstract-Keyword-in-Java/src/Main.java
@@ -0,0 +1,24 @@
+
+abstract class Animal {
+    public abstract void sound(); // Every animal can have different sound so you cant define this method here --> This is called abstraction
+
+}
+
+class Dog extends Animal {
+    public void sound() {
+        System.out.println("Bark"); // Give the definition here
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Animal obj=new Dog(); // You have to create the object of the subclass 
+        obj.sound();
+    }
+}
+
+// Abstract Keyword
+
+// It is a part of Java's Object-Oriented Programming (OOP) principles to provide partial implementation while leaving  the details to be implemented by subclasses.
+// To make a method abstract you have to create the class as abstract
+// and you cant create the object of that class but can create the object reference
