de1ac6f8cb4b498df47c732cea91926933a76957
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-22 12:17:27 +0530

    Refactoring code in Access modifiers

--- java/10-Access-Modifiers-in-Java/src/Access.java
+++ java/10-Access-Modifiers-in-Java/src/Access.java
@@ -2,11 +2,11 @@ import packageA.A;
 
 public class Access {
     public static void main(String[] args) {
-        A obj=new A();
+        A obj = new A();
 
         System.out.println(obj.name); // This is another package therefore we have to make it public
 
-        B obj1=new B();
+        B obj1 = new B();
 
         System.out.println(obj1.age); // its in the same package that why it is by default public
     }
@@ -15,6 +15,6 @@ public class Access {
 // Access Modifiers
 
 // 1. Public --> Available in every files and packages
-// 2. Default --> Available in same class, same package non child class, same package child class
-// 3. Protected --> Available in same class, same package no child class, same package child class, different package child class
+// 2. Default --> Available in same class, same package
+// 3. Protected --> Available in same class, same package, different package but child class
 // 4. Private --> Available in same class only
