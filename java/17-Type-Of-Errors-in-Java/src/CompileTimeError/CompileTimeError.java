b19022c3773b6087f9f2bbb3da26cad37da7382e
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-23 09:59:07 +0530

    Compile Time Errors

---
+++ java/17-Type-Of-Errors-in-Java/src/CompileTimeError/CompileTimeError.java
@@ -0,0 +1,17 @@
+package CompileTimeError;
+
+public class CompileTimeError {
+    public static void main(String[] args) {
+//        System.out.Println("Hello, World!"); --> the Println will give the Compile Time Error by the compiler
+    }
+}
+
+// Errors in Java
+
+// There are three types of errors
+
+// 1. Compile Time Error --> When code gives the error after we compile the code is called Compile Time Error, It is handled by the compiler.
+
+// 2. Runtime Error --> When after compiling the code we run the code and the error comes it's called Runtime Error, It should be handled by the developer and Its also called as Exception.
+
+// 3. Logical Error --> When the developer has done some logical mistake in the code is logical Error.
