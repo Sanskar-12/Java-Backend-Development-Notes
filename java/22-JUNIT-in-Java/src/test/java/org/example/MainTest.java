20823dd6bcbe5b3098d852b4afefe2795a161a89
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-28 12:41:39 +0530

    Test Case to divide numbers

--- java/22-JUNIT-in-Java/src/test/java/org/example/MainTest.java
+++ java/22-JUNIT-in-Java/src/test/java/org/example/MainTest.java
@@ -5,11 +5,30 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
+    // Way to create test case
     @Test
     public void test() {
         Main obj=new Main();
         int actual=obj.divide(10,2);
         int expectations=5;
-        Assertions.assertEquals(expectations,actual);
+        Assertions.assertEquals(expectations,actual); // Verifying the actual value with expectated value
+    }
+
+    // The Message would be shown in any condition
+    @Test
+    public void test2withMessage() {
+        Main obj=new Main();
+        int actual=obj.divide(10,2);
+        int expectations=5;
+        Assertions.assertEquals(expectations,actual,"Message to dev it test case fails");
+    }
+
+    // The Message would be shown if the test case fails
+    @Test
+    public void test3ifTestCaseFailMessage() {
+        Main obj=new Main();
+        int actual=obj.divide(10,2);
+        int expectations=5;
+        Assertions.assertEquals(expectations,actual,()->"Message to dev it test case fails");
     }
 }
