c181c0d0fcfed41df8da4a84fb9ece9abaaac355
Sanskar-12 <sanskarv2004@gmail.com>
2025-01-28 11:22:44 +0530

    First Test Case Run using JUNIT

---
+++ java/22-JUNIT-in-Java/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public int divide(int num1,int num2) {
+        return num1/num2;
+    }
+}
---
+++ java/22-JUNIT-in-Java/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,12 @@
+package $org.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static int divide(int num1, int num2) {
+
+        return num1 / num2;
+
+    }
+}
