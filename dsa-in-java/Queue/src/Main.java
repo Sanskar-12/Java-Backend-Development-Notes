f4945bb0197c787fab704d6f1285858ebe85b0ec
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-07 17:02:43 +0530

    More methods in queue

--- dsa-in-java/Queue/src/Main.java
+++ dsa-in-java/Queue/src/Main.java
@@ -6,6 +6,9 @@ public class Main {
         queue.enqueue(3);
         queue.enqueue(4);
         queue.enqueue(5);
+        queue.enqueue(0);
+        queue.enqueue(8);
+        queue.enqueue(8);
 
         // delete from queue
         System.out.println(queue.dequeue() + " is Removed");
