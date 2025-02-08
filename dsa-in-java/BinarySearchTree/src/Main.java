c77001bbae2da9e6b13bf88ac92c6d991b12fb31
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-08 17:28:18 +0530

    Binary search tree insert and inorder traversal methods

---
+++ dsa-in-java/BinarySearchTree/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree b=new BinarySearchTree();
+
+        b.insert(8);
+        b.insert(7);
+        b.insert(5);
+        b.insert(10);
+
+        b.inorderShow();
+    }
+}
