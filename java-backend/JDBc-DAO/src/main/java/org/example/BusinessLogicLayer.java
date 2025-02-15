7fd4aef781540a23d505e490282eebf263e5d2be
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-15 17:53:42 +0530

    JDBC Data Access Object Example 1

---
+++ java-backend/JDBc-DAO/src/main/java/org/example/BusinessLogicLayer.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class BusinessLogicLayer {
+    public static void main(String[] args) throws Exception {
+        DatabaseInteractionLayer databaseInteractionLayer = new DatabaseInteractionLayer();
+        DataVariablesStorageLayer s = databaseInteractionLayer.getStudentName(7);
+
+        System.out.println(s.name);
+    }
+}
+
+// DAO - (Data Access Object)
+
+// In JDBC, DAO (Data Access Object) is a design pattern used to separate database interactions from business logic. It provides a structured way to interact with a database using CRUD operations.
+
+// For eg
+// Suppose i have to fetch the name of student whose roll no is 11
+// So i will make a class in which i will call the method for fetching the name from roll no
+// and i will make another class in which the method will be written for fetching the name from roll no in which it will contain database interaction
+
+// so i have made a layer between database interaction and main business logic
---
+++ java-backend/JDBc-DAO/src/main/java/org/example/DataVariablesStorageLayer.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class DataVariablesStorageLayer {
+    int userId;
+    String name;
+}
