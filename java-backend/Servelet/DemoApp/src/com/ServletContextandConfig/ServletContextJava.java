451163326eff62b6b5fe3cfb84ca598b9e7deed2
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-18 17:06:51 +0530

    Servlet Configuration with Annotations

--- java-backend/Servelet/DemoApp/src/com/ServletContextandConfig/ServletContextJava.java
+++ java-backend/Servelet/DemoApp/src/com/ServletContextandConfig/ServletContextJava.java
@@ -5,10 +5,12 @@ import java.io.PrintWriter;
 
 
 import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/ctx") // --> This defines that if someone request on ctx url then it should got to this Servlet
 public class ServletContextJava extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		PrintWriter out = res.getWriter();
