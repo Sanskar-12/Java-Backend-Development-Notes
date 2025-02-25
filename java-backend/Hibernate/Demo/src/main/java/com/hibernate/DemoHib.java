473d46d3e0c5669e871b26c063902b7e5aff7572
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-25 09:45:20 +0530

    show sql property - how hibernate write its sql queries

--- java-backend/Hibernate/Demo/src/main/java/com/hibernate/DemoHib.java
+++ java-backend/Hibernate/Demo/src/main/java/com/hibernate/DemoHib.java
@@ -8,10 +8,10 @@ import org.hibernate.cfg.Configuration;
 public class DemoHib {
 	public static void main(String[] args) {
 		Alien telusko = new Alien();
-		telusko.setAid(3);
-		telusko.setAname("Anjali");
+		telusko.setAid(4);
+		telusko.setAname("Ishu");
 		telusko.setAcolor("red");
-		telusko.setAemail("anjali@gmail.com");
+		telusko.setAemail("ishu@gmail.com");
 		
 		Configuration config = new Configuration().configure().addAnnotatedClass(Alien.class);
 		
