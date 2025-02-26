cea35bc21c48b81141489643b12a5339795087e1
Sanskar-12 <sanskarv2004@gmail.com>
2025-02-26 13:32:09 +0530

    Relationship in Hibernate

---
+++ java-backend/Hibernate/relationshipDemo/src/main/java/com/hibernate/Laptop.java
@@ -0,0 +1,29 @@
+package com.hibernate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Laptop {
+	@Id
+	private int lid;
+	private String lname;
+	public int getLid() {
+		return lid;
+	}
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	
+	@Override
+	public String toString() {
+		return "Laptop [lid=" + lid + ", lname=" + lname + "]";
+	}
+	
+}
