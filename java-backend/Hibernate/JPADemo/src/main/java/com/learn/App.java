fabf939906aacfe41d2af575e4bd52eee193ed8e
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-06 12:00:11 +0530

    JPA persist method

--- java-backend/Hibernate/JPADemo/src/main/java/com/learn/App.java
+++ java-backend/Hibernate/JPADemo/src/main/java/com/learn/App.java
@@ -6,11 +6,25 @@ import jakarta.persistence.Persistence;
 
 public class App {
 	public static void main(String[] args) {
+		
+		Student s = new Student();
+		s.setName("Sanskar");
+		s.setMarks(20);
+		s.setRollno(60);
+		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
 		EntityManager em = emf.createEntityManager();
-		Student a = em.find(Student.class, 1);
+//		Student a = em.find(Student.class, 1); // get the value from the database
 		
-		System.out.println(a);
+		
+		// To save the value inside database we have to start the transaction and then commit it
+		em.getTransaction().begin();
+		
+		em.persist(s); // Save the value to the database
+		
+		em.getTransaction().commit();
+		
+		System.out.println(s);
 	}
 }
 
