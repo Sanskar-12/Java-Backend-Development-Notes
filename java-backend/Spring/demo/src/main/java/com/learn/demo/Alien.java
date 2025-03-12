24323192e6595ee112330f62648c4e01946df392
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-12 13:30:20 +0530

    Singleton vs Prototype Design pattern of spring

--- java-backend/Spring/demo/src/main/java/com/learn/demo/Alien.java
+++ java-backend/Spring/demo/src/main/java/com/learn/demo/Alien.java
@@ -1,6 +1,9 @@
 package com.learn.demo;
 
 public class Alien {
+	
+	public int age = 10;
+	
 	public void code() {
 		System.out.println("Coding");
 	}
--- java-backend/Spring/demo/src/main/java/com/learn/demo/App.java
+++ java-backend/Spring/demo/src/main/java/com/learn/demo/App.java
@@ -6,10 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
@@ -18,8 +15,23 @@ public class App
     	
     	ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml"); // Replacement of XMLBeanFactory
     	
-        Alien obj = (Alien)factory.getBean("alien");
+        Alien obj1 = (Alien)factory.getBean("alien");
         
-        obj.code();
+        obj1.age = 20;
+        
+        obj1.code();
+        System.out.println(obj1.age); // output will be 20
+        
+        Alien obj2 = (Alien)factory.getBean("alien");
+        
+        obj2.code();
+        System.out.println(obj2.age); // output will be 20
     }
 }
+
+
+// Sigleton deisgn pattern of spring
+// Singleton means all the object we create refer to same variable 
+// It means of we a create an object it will not create a new object but it will refer to the old object variable
+
+// Prototype scope will create multiple objects you have to define the scope inside spring.xml file
