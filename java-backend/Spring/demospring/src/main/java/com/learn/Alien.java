c42b21e63a183b2afcdcf216b5cbf3afeed620a6
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-12 17:06:34 +0530

    Property inside spring

---
+++ java-backend/Spring/demospring/src/main/java/com/learn/Alien.java
@@ -0,0 +1,14 @@
+package com.learn;
+
+public class Alien {
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		System.out.println("Set Age");
+		this.age = age;
+	}
+}
---
+++ java-backend/Spring/demospring/src/main/java/com/learn/Demo.java
@@ -0,0 +1,20 @@
+package com.learn;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+public class Demo {
+	public static void main(String[] args) {
+		ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml"); 
+    	
+		Alien obj1 = (Alien)factory.getBean("alien");
+        
+        System.out.println(obj1.getAge());
+	}
+}
+
+// We can set the initial value of age var which is private inside the property inside spring.xml file
+// It will by default call the setAge method to set the value
+// property is used to set the default value inside the class variable which is private
