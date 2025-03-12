e9ebd915a214a380141eb3e075ca92dc6a8758f8
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-12 13:04:15 +0530

    Object creation using spring Application Context

--- java-backend/Spring/demo/src/main/java/com/learn/demo/App.java
+++ java-backend/Spring/demo/src/main/java/com/learn/demo/App.java
@@ -2,6 +2,8 @@ package com.learn.demo;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 /**
@@ -12,7 +14,9 @@ public class App
 {
     public static void main( String[] args )
     {
-    	BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	// BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml")); // Deprecated XMLBeanFactory
+    	
+    	ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml"); // Replacement of XMLBeanFactory
     	
         Alien obj = (Alien)factory.getBean("alien");
         
