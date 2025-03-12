af164cdc1145528e4a16ce36c0bd37e5c1f0a599
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-12 17:16:36 +0530

    Ref in spring

--- java-backend/Spring/demospring/src/main/java/com/learn/Alien.java
+++ java-backend/Spring/demospring/src/main/java/com/learn/Alien.java
@@ -2,8 +2,18 @@ package com.learn;
 
 public class Alien {
 	private int age;
+	private Laptop laptop;
+
+	public Laptop getLaptop() {
+		return laptop;
+	}
+
+	public void setLaptop(Laptop laptop) {
+		this.laptop = laptop;
+	}
 
 	public int getAge() {
+		laptop.code();
 		return age;
 	}
 
