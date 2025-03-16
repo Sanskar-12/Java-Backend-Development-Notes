d9c0c79a92bf99d79a7bc08159b23a8cb1f5d75c
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-16 12:46:40 +0530

    Adding two numbers and displaying on page

--- java-backend/SpringMVC/demo/src/main/java/com/example/demo/HomeController.java
+++ java-backend/SpringMVC/demo/src/main/java/com/example/demo/HomeController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
@@ -9,5 +10,10 @@ public class HomeController {
 	@GetMapping("/")
 	public String home() {
 		return "index.jsp";
+	}
+	
+	@RequestMapping("add")
+	public String add() {
+		return "result.jsp";
 	}
 }
--- java-backend/SpringMVC/demo/src/main/webapp/result.jsp
+++ java-backend/SpringMVC/demo/src/main/webapp/result.jsp
@@ -6,12 +6,6 @@
     <title>JSP Starter Page</title>
 </head>
 <body>
-    Welcome to Calculator
-	
-	<form action="add">
-		Enter num1 : <input type="text" name="num1"/> <br/>
-		Enter num2 : <input type="text" name="num2"/> <br/>
-		<input type="submit"/>
-	</form>
+   Result is : 
 </body>
 </html>
