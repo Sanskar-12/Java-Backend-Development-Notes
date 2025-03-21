e14ce82f5c04ca902fe42427f0bd421df6680f43
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-21 12:32:30 +0530

    Dynamic Param find by id

--- java-backend/SpringMVC/demo/src/main/java/com/example/demo/AlienController.java
+++ java-backend/SpringMVC/demo/src/main/java/com/example/demo/AlienController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.model.Alien;
@@ -21,6 +22,15 @@ public class AlienController {
 		List<Alien> aliens = repo.findAll();
 		
 		return aliens;
+	}
+	
+	// Params 
+	@GetMapping("alien/{aid}") // aid is dynamic
+	@ResponseBody
+	public Alien getAlien(@PathVariable("aid") int aid) {
+		Alien alien = repo.findById(aid).orElse(new Alien(0,""));
+		
+		return alien;
 	}
 	
 }
