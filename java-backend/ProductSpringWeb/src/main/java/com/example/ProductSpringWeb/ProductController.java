26d50bcf007fe8d1e36e159fbab7a699cd6c70bf
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-24 12:05:45 +0530

    Spring Web MVC Get products

---
+++ java-backend/ProductSpringWeb/src/main/java/com/example/ProductSpringWeb/ProductController.java
@@ -0,0 +1,20 @@
+package com.example.ProductSpringWeb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductService service;
+
+    @GetMapping("/products") // products route will call below method
+    public List<Product> getAllProducts() {
+        return service.getAllProducts();
+    }
+
+}
