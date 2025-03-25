3f6338f4815729a980b1188b1d41fb04ef54c85b
Sanskar-12 <sanskarv2004@gmail.com>
2025-03-25 12:10:38 +0530

    made get all posts method for finding all posts

--- java-backend/jobfinder/src/main/java/com/example/jobfinder/controller/PostController.java
+++ java-backend/jobfinder/src/main/java/com/example/jobfinder/controller/PostController.java
@@ -1,4 +1,21 @@
 package com.example.jobfinder.controller;
 
+import com.example.jobfinder.PostRepository;
+import com.example.jobfinder.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class PostController {
+
+    @Autowired
+    PostRepository repo;
+
+    @GetMapping("/posts")
+    public List<Post> getAllPosts() {
+        return repo.findAll();
+    }
 }
